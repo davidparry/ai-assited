80ba62278b4233ea803665416e6d0e005c4c758a
davidparry <david.p@codium.ai>
2025-04-23 14:11:46 -0500

    added system out println

--- src/main/java/ai/qodo/joke/LogCleaner.java
+++ src/main/java/ai/qodo/joke/LogCleaner.java
@@ -5,6 +5,7 @@ public class LogCleaner implements Scrubber {
 
     @Override
     public String scrub(String joke) {
+        System.out.println("Just logging the joke in a system out :-( "+ joke);
         return null;
     }
 }
